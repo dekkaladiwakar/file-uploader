92142e2642aae586631f6c3a3b7956f1b483b826
dekkaladiwakar <dekkaladiwakar@gmail.com>
2023-10-09 14:29:02 +0530

    Add channel/worker to handle multi request load

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"strconv"
@@ -11,15 +12,46 @@ import (
 	"github.com/rs/cors"
 )
 
+const maxWorkers = 10
+const maxQueueSize = 1000
+
+type ChunkRequest struct {
+	File      multipart.File
+	FileID    string
+	ChunkNum  int
+	StartByte int64
+	EndByte   int64
+}
+
+var chunkQueue = make(chan ChunkRequest, maxQueueSize)
+
+func worker(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for chunk := range chunkQueue {
+		chunkFilename := fmt.Sprintf("%s-chunk%d", chunk.FileID, chunk.ChunkNum)
+
+		dst, err := os.Create(chunkFilename)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		chunk.File.Seek(chunk.StartByte, 0)
+		buf := make([]byte, chunk.EndByte-chunk.StartByte+1)
+		chunk.File.Read(buf)
+		dst.Write(buf)
+		dst.Close()
+	}
+}
+
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
-	// Parse the multipart form
-	err := r.ParseMultipartForm(10 << 20) // limit your max memory size
+	err := r.ParseMultipartForm(10 << 20)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	// Get the file data, and thread count from the request
 	file, _, err := r.FormFile("file")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -30,78 +62,63 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	threadCountStr := r.FormValue("threads")
 	threadCount, err := strconv.Atoi(threadCountStr)
 	if err != nil || threadCount <= 0 {
-		http.Error(w, "Invalid thread count", http.StatusBadRequest)
+		http.Error(w, "Invalid thread count, must be between greater than 1", http.StatusBadRequest)
 		return
 	}
 
 	fileID := r.FormValue("fileID")
-
 	if fileID == "" {
-		http.Error(w, "FileId is missing", http.StatusBadRequest)
+		http.Error(w, "Missing fileID", http.StatusBadRequest)
 		return
 	}
 
-	// Get the file size to determine chunk size
 	fileSize := r.ContentLength
 	chunkSize := fileSize / int64(threadCount)
 
-	var wg sync.WaitGroup
-
 	for i := 0; i < threadCount; i++ {
-		wg.Add(1)
+		startByte := int64(i) * chunkSize
+		endByte := int64(0)
 
-		go func(threadNum int) {
-			defer wg.Done()
+		if i == threadCount-1 {
+			endByte = fileSize
+		} else {
+			endByte = startByte + chunkSize - 1
+		}
+		chunk := ChunkRequest{
+			File:      file,
+			FileID:    fileID,
+			ChunkNum:  i + 1,
+			StartByte: startByte,
+			EndByte:   endByte,
+		}
 
-			// Determine the byte range for this chunk
-			startByte := int64(threadNum) * chunkSize
-			endByte := startByte + chunkSize - 1
-			if threadNum == threadCount-1 {
-				endByte = fileSize // ensure the last chunk goes to the end of the file
-			}
-
-			// Create a unique filename for this chunk
-			chunkFilename := fmt.Sprintf("%s-chunk%d", fileID, threadNum)
-
-			// Create a new file to store the chunk
-			dst, err := os.Create(chunkFilename)
-			if err != nil {
-				log.Println(err) // Log the error and continue
-				return
-			}
-			defer dst.Close()
-
-			// Seek to the start byte and read the chunk data from the file
-			file.Seek(startByte, 0)
-			buf := make([]byte, endByte-startByte+1)
-			file.Read(buf)
-
-			// Write the chunk data to the file
-			dst.Write(buf)
-
-		}(i)
+		chunkQueue <- chunk
 	}
 
-	wg.Wait() // Wait for all goroutines to finish
-
-	fmt.Fprintf(w, "File %s uploaded and processed successfully with %d threads", fileID, threadCount)
+	fmt.Fprintf(w, "File %s upload initiated with %d threads", fileID, threadCount)
 }
 
 func main() {
-	// Create a new router
 	router := http.NewServeMux()
-
-	// Register the chunk upload handler
 	router.HandleFunc("/upload", UploadHandler)
 
-	// Set up CORS middleware
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+
 	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"*"}, // Allow all origins
+		AllowedOrigins: []string{"*"},
 		AllowedMethods: []string{"POST"},
 	})
 
+	var wg sync.WaitGroup
+	for i := 0; i < maxWorkers; i++ {
+		wg.Add(1)
+		go worker(i, &wg)
+	}
+
 	log.Println("Server is starting on Port 8080.....")
-	// Start the server with CORS middleware
 	handler := c.Handler(router)
 	http.ListenAndServe(":8080", handler)
+
+	close(chunkQueue)
+	wg.Wait()
 }
